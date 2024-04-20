4ad4ccb34eedc2f3758080f72f24e4cb2cfe60ac
ruanjiancheng <1434919953@qq.com>
2024-04-21 01:21:16 +0800

    Feat range search func and hash func

--- include/core.h
+++ include/core.h
@@ -45,8 +45,8 @@ const int EF_CONSTRUCTION_Q0123 = 140;
 // const int HNSW_MERGE_THRASHOLD = 100000;
 
 const int EF_SEARCH_Q0 = 512 + 72;
-const int EF_SEARCH_Q1 = 612 + 64;
-const int EF_SEARCH_Q2 = 256;
+const int EF_SEARCH_Q1 = 512;
+const int EF_SEARCH_Q2 = 256 + 64;
 const int EF_SEARCH_Q3 = 256;
 
 const int HNSW_BUILD_THRASHOLD = 500;
